f477abe4ac1029f868f62b20a9b86fb06ddfb7f9
Agustin Barrachina <joseagustin.barra@gmail.com>
2017-02-08 19:58:37 +0100

    Added a compared bash to see if images are well done

---
+++ images/compare.sh
@@ -0,0 +1,15 @@
+composite originalFiltered/051009.vince-sobel.gif processed/051009.vince-sobel.gif -compose difference compared/051009.vince-sobel.gif
+composite originalFiltered/1-sobel.gif processed/1-sobel.gif -compose difference compared/1-sobel.gif
+composite originalFiltered/200_s-sobel.gif processed/200_s-sobel.gif -compose difference compared/200_s-sobel.gif
+composite originalFiltered/9815573-sobel.gif processed/9815573-sobel.gif -compose difference compared/9815573-sobel.gif
+composite originalFiltered/australian-flag-large-sobel.gif processed/australian-flag-large-sobel.gif -compose difference compared/australian-flag-large-sobel.gif
+composite originalFiltered/Campusplan-Hausnr-sobel.gif processed/Campusplan-Hausnr-sobel.gif -compose difference compared/Campusplan-Hausnr-sobel.gif
+composite originalFiltered/Campusplan-Mobilitaetsbeschraenkte-sobel.gif processed/Campusplan-Mobilitaetsbeschraenkte-sobel.gif -compose difference compared/Campusplan-Mobilitaetsbeschraenkte-sobel.gif
+composite originalFiltered/fire-sobel.gif processed/fire-sobel.gif -compose difference compared/fire-sobel.gif
+composite originalFiltered/frame_002-sobel.gif processed/frame_002-sobel.gif -compose difference compared/frame_002-sobel.gif
+composite originalFiltered/giphy-3-sobel.gif processed/giphy-3-sobel.gif -compose difference compared/giphy-3-sobel.gif
+composite originalFiltered/Mandelbrot-large-sobel.gif processed/Mandelbrot-large-sobel.gif -compose difference compared/Mandelbrot-large-sobel.gif
+composite originalFiltered/porsche-sobel.gif processed/porsche-sobel.gif -compose difference compared/porsche-sobel.gif
+composite originalFiltered/TimelyHugeGnu-sobel.gif processed/TimelyHugeGnu-sobel.gif -compose difference compared/TimelyHugeGnu-sobel.gif
+composite originalFiltered/tumblr_myxlbtwVEb1qzt4vjo1_r14_500_large-sobel.gif processed/tumblr_myxlbtwVEb1qzt4vjo1_r14_500_large-sobel.gif -compose difference compared/tumblr_myxlbtwVEb1qzt4vjo1_r14_500_large-sobel.gif
+

deb7f018b5cfbd88811e1aa3df14eee472dc59a7
Agustin Barrachina <joseagustin.barra@gmail.com>
2017-03-14 15:58:12 +0100

    Creation of final TD branch

---
+++ images/compare.sh
@@ -0,0 +1,29 @@
+compare -metric AE originalFiltered/051009.vince-sobel.gif processed/051009.vince-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/1-sobel.gif processed/1-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/200_s-sobel.gif processed/200_s-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/9815573-sobel.gif processed/9815573-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/australian-flag-large-sobel.gif processed/australian-flag-large-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/Campusplan-Hausnr-sobel.gif processed/Campusplan-Hausnr-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/Campusplan-Mobilitaetsbeschraenkte-sobel.gif processed/Campusplan-Mobilitaetsbeschraenkte-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/fire-sobel.gif processed/fire-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/frame_002-sobel.gif processed/frame_002-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/giphy-3-sobel.gif processed/giphy-3-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/Mandelbrot-large-sobel.gif processed/Mandelbrot-large-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/porsche-sobel.gif processed/porsche-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/TimelyHugeGnu-sobel.gif processed/TimelyHugeGnu-sobel.gif null
+echo "$STR"
+compare -metric AE originalFiltered/tumblr_myxlbtwVEb1qzt4vjo1_r14_500_large-sobel.gif processed/tumblr_myxlbtwVEb1qzt4vjo1_r14_500_large-sobel.gif null
+echo "$STR"
+
